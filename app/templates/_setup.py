e6a534c25fd8d32e86d0f8569f252535d46d40ea
xiaogaozi <gaochangjian@gmail.com>
2014-11-27 20:12:02 +0800

    Add classifier

--- app/templates/_setup.py
+++ app/templates/_setup.py
@@ -14,5 +14,16 @@ setup(
     author_email='<%= user.git.email %>',
     packages=find_packages(exclude=['tests']),
     package_data={'<%= pkg %>': ['CHANGES', 'README.md']},
-    zip_safe=False,
+    zip_safe=False,<% if (setClassifier) { %>
+    classifier=[<% _.each(classifierDevelopmentStatus, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierEnvironment, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierFramework, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierIntendedAudience, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierLicense, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierNaturalLanguage, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierOperatingSystem, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierProgrammingLanguage, function(o) { %>
+        '<%= o %>',<% }); %><% _.each(classifierTopic, function(o) { %>
+        '<%= o %>',<% }); %>
+    ],<% } %>
 )
